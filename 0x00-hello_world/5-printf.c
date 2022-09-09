e76197a7de0ffc1efe10cd83c5dc37774d78dff1
minnieme20 <minniethuo@protonmail.com>
2022-09-09 12:44:55 -0700

    task4and5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - printing text
+ *
+ * Return: Always 0 (succesful)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return(0);
+}
