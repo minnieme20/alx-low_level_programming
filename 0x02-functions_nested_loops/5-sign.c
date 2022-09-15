3e62ee14f060674987ecd83c208066633bd51ba8
minnieme20 <minniethuo@protonmail.com>
2022-09-15 14:52:42 -0700

    correction

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_sign - function to check for a sign of a number
- * @n: is the int that will use for the argument of the function 
+ * @n:  is the int that will use for the argument of the function
  * Return: 0
  */
 int print_sign(int n)
@@ -11,15 +11,13 @@ int print_sign(int n)
 	_putchar('+');
 	return (1);
 }
-	
 	else if (n < 0)
 	{
 	_putchar('-');
-	return (-1);
+	return (-1);   
 	}
 	else
 	{
 	_putchar('0');
 	return (0);
-	}
 }
