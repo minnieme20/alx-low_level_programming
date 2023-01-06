ef7d667a32551e4930725559be62402d16371938
minnieme20 <minniethuo@protonmail.com>
2023-01-06 09:53:51 -0800

    task0

--- 0x08-recursion/_putchar.c
+++ 0x08-recursion/_putchar.c
@@ -3,7 +3,6 @@
                                                                                                                                         
 /**                                                                                                                                     
  * _putchar - writes the character c to stdout                                                                                           */                                                                                                                                     
-
 int _putchar(char c)                                                                                                                    
 {                                                                                                                                       
         return (write(1, &c, 1));                                                                                                       
