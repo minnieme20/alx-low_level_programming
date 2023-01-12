b506dc4cdf78f4fb4ee9f14aeba7d7dadfce587f
minnieme20 <minniethuo@protonmail.com>
2023-01-12 05:31:55 -0800

    task2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main -> this is a function to print its name 
+ * main -> this is a function to print its name
  * @argc: argc parameter
  * @argv: an array of a command listed
  * Return: 0 for success
